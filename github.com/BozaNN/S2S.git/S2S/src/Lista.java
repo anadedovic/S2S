2629d0f6ebd1c1091a416a5a1d32cccc0dab88bb
akikasaki <alektramp@gmail.com>
2016-12-12 21:21:07 +0100

    a

--- github.com/BozaNN/S2S.git/S2S/src/Lista.java
+++ github.com/BozaNN/S2S.git/S2S/src/Lista.java
@@ -3,9 +3,5 @@ import java.util.LinkedList;
 public class Lista {
 	public LinkedList<Student> studenti = new LinkedList<Student>();
 	
-	public void napraviListu(Student s){
-		if(s==null){
-			syso
-		}
-	}
+
 }
